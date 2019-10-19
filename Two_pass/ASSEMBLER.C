e18e0e45f315d007b485444a898fb3ab0fbfadb8
AquaRegis32 <ananduraj32@gmail.com>
2019-10-19 05:52:41 +0530

    completed one pass

--- Two_pass/ASSEMBLER.C
+++ Two_pass/ASSEMBLER.C
@@ -10,12 +10,10 @@ void refresh(){
 	fprintf(b,"");
 	fclose(a);
 	fclose(b);	
-}
-
-int main(){
+}int main(){
 	char opcode[10],label[10],operand[20],symbol[10],op[10];
 	FILE *f1,*f2,*f3,*f4;
-	int start,LOCCTR=0,flag=0,len=0;
+	int start,LOCCTR=0,flag=0,len=0,loc=0;
 	refresh();
 	f1=fopen("INPUT.DAT","r");
 	f2=fopen("OUTPUT.DAT","a");
@@ -24,14 +22,14 @@ int main(){
 	fscanf(f1,"%s%s%s",label,opcode,operand);
 	if(strcmp(opcode,"START")==0){
 		if(atoi(operand)>0)
-			LOCCTR=start=atoi(operand);
+			loc=LOCCTR=start=atoi(operand);
 		printf("\nSTARTING ADDRESS= %d\n",start);		
 		fprintf(f2,"\t\t%s\t%s\t%s\n",label,opcode,operand);
 		fscanf(f1,"%s%s%s",label,opcode,operand);
 		do{
 			
 			flag=0;
-			if(isalpha(label[0])){
+			if(!isalpha(label[0])){
 				LOCCTR+=3;
 				do{
 					fscanf(f4,"%s",symbol);
@@ -48,7 +46,7 @@ int main(){
 					if(strcmp(opcode,op)==0){
 						LOCCTR+=3;
 						flag=1;
-						break;
+						continue;
 					}
 				}while(!EOF);
 				rewind(f3);
@@ -56,12 +54,11 @@ int main(){
 					if(strcmp(opcode,"WORD")==0)
 						LOCCTR+=3;
 					else if(strcmp(opcode,"BYTE")==0){
-						if(op[0]=='X')
-							LOCCTR+=1;
-						else{
+						if(op[0]=='X'){
 							len=strlen(opcode)-2;
 							LOCCTR+=len;
-						}
+						}else
+							LOCCTR+=1;
 					}
 					else if(strcmp(opcode,"RESW")==0)
 						LOCCTR+=3*atoi(operand);
@@ -71,11 +68,11 @@ int main(){
 						printf("\n\tINVLAID OPCODE\t%d\n",LOCCTR);
 				}
 				
-			}fprintf(f2,"%d\t%s\t%s\n",LOCCTR,label,opcode,operand);
+			}fprintf(f2,"%d\t%s\t%s\t%s\n",loc,label,opcode,operand);
+			loc=LOCCTR;
 			fscanf(f1,"%s%s%s",label,opcode,operand);
 		}while(strcmp(opcode,"END")!=0);
-		
+		fprintf(f2,"%d",LOCCTR);		
 		printf("\nPROGRAM LENGTH= %d\n\n",LOCCTR-start);
-	}	
-	return 0;
+	}return 0;
 }
