f6733a3117c42f106e59d4f1ba61671ba9a5c498
AquaRegis32 <Ananduraj32@gmail.com>
2019-11-07 11:55:35 +0530

    nov7_two_pass-pass_two#3

---
+++ Two_pass/Abs_loader/ASSEMBLER.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+void main()
+{
+    FILE *f1,*f3,*f2,*f4;
+    int op1[10],txtlen,txtlen1,i,j=0,len;
+    char addr[5],symaddr[5],op[5],start[10],temp[30],line[20],label[20],mne[10],operand[10],symtab[10],opmne[10];
+    f1=fopen("INPUT.txt","r");
+    f2=fopen("LENGTH.txt","r");
+    f3=fopen("OPTAB.txt","r");
+    f4=fopen("SYMBOL.txt","r");
+    fscanf(f1,"%s%s%s%s",addr,label,mne,operand);
+    if(strcmp(mne,"START")==0)
+    {
+        strcpy(start,operand);
+        fscanf(f2,"%d",&len);
+    }
+    printf("H^%s^%s^%d\nT^00%s^",label,start,len,start);
+    fscanf(f1,"%s%s%s%s",addr,label,mne,operand);
+    while(strcmp(mne,"END")!=0)
+    {
+        fscanf(f3,"%s%s",opmne,op);
+        while(!feof(f3))
+        {
+            if(strcmp(mne,opmne)==0)
+            {
+                fclose(f3);
+                fscanf(f4,"%s%s",symaddr,symtab);
+                while(!feof(f4))
+                {
+                    if(strcmp(operand,symtab)==0)
+                    {
+                        printf("%s%s^",op,symaddr);
+                        break;
+                    }
+                    else
+                        fscanf(f4,"%s%s",symaddr,symtab);
+                }
+                break;
+            }
+            else
+                fscanf(f3,"%s%s",opmne,op);
+        }
+        if((strcmp(mne,"BYTE")==0)||(strcmp(mne,"WORD")==0))
+        {
+            if(strcmp(mne,"WORD")==0)
+            printf("0000%s^",operand);
+            else
+            {
+                len=strlen(operand);
+                for(i=2;i<len;i++)
+                {
+                    printf("%d",operand[i]);
+                }
+                printf("^");
+            }
+        }
+        fscanf(f1,"%s%s%s%s",addr,label,mne,operand);
+        f3=fopen("OPTAB.txt","r");
+        fseek(f3,SEEK_SET,0);
+    }
+    printf("\nE^00%s\n",start);
+    fclose(f1);
+    fclose(f3);
+    fclose(f4);
+    fclose(f2);
+}
